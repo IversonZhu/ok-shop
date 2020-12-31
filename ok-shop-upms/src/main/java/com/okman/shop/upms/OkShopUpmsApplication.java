b649b39e84daca1caa5443dbfbf70846667902c3
zhuhuayao <zhuhuayao930816>
2020-12-31 17:35:15 +0800

    fix:修改依赖文件

---
+++ ok-shop-upms/src/main/java/com/okman/shop/upms/OkShopUpmsApplication.java
@@ -0,0 +1,13 @@
+package com.okman.shop.upms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkShopUpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkShopUpmsApplication.class, args);
+    }
+
+}
---
+++ ok-shop-upms/src/test/java/com/okman/shop/upms/OkShopUpmsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.okman.shop.upms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkShopUpmsApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
